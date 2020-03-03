ac07b5121ce752a649aab8c659b0c8e340f9e743
ranckprogram <1474574022@qq.com>
2020-03-03 11:38:50 +0800

    test

--- src/services/index.js
+++ src/services/index.js
@@ -220,7 +220,7 @@ export default {
         return axios.post(`${nodeBaseUrl}/linkingthing/node/v1/nodes?${params2Query(params)}`)
     },
     getServerList () {
-        return axios.get(`${nodeBaseUrl}/linkingthing/node/v1/servers`)
+        return axios.get(`${nodeBaseUrl}/linkingthing/node1/v1/servers`)
     },
     getDeviceHistoryInfo(params) {
         return axios.post(`${nodeBaseUrl}/linkingthing/node/v1/hists?${params2Query(params)}`)
