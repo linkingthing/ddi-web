f546031d5ce43e48445fa5090f20b71e279bfb6e
dontknowly <314589318@qq.com>
2020-05-27 15:26:30 +0800

    修复BUG 151 ： IPAM-IP地址规划无数据的情况与设计图不一致

---
+++ src/components/NoDataList/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="no-data-list" :style="{marginTop: getTop}">
+    <img :src="img" >
+
+    <p>{{text}}</p>
+
+    <Button
+      v-if="showButton"
+      type="primary"
+      @click="$emit('add')"
+    >
+      {{buttonText}}
+    </Button>
+  </div>
+</template>
+
+<style lang="less">
+@import "./index.less";
+</style>
+
+<script>
+import img from "@/assets/images/no-data-1.png";
+
+export default {
+  name: "ListNoData",
+
+  props: {
+    buttonText: {
+      type: String,
+      default: "添加"
+    },
+
+    text: {
+      type: String,
+      default: "还没有数据哦，快去添加吧！"
+    },
+
+    showButton: {
+      type: Boolean,
+      default: true
+    },
+
+    top: {
+      type: [String, Number],
+      default: 0
+    }
+  },
+
+  data() {
+    return {
+      img
+    };
+  },
+
+  computed: {
+    getTop() {
+      return parseFloat(this.top) + "px";
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
