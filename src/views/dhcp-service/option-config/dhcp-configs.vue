abb67a1e29dcffc64c57f9338c5272272b438505
ranckprogram <1474574022@qq.com>
2020-06-05 10:17:44 +0800

    dhcp new create subnet

--- src/views/dhcp-service/option-config/dhcp-configs.vue
+++ src/views/dhcp-service/option-config/dhcp-configs.vue
@@ -122,7 +122,7 @@ export default {
   mounted() { },
   methods: {
     handleSubmit(name) {
-      this.$refs[name].validate((valid) => {
+      this.$refs[name].validate(valid => {
 
         if (valid) {
           const params = this.formModel;
