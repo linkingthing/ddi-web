49144148c64ea22480fdc1541088c3f1bbd4ea16
ranckprogram <1474574022@qq.com>
2020-07-17 10:22:55 +0800

    system log detail show

--- src/views/system/log/auditlog/define.js
+++ src/views/system/log/auditlog/define.js
@@ -1,25 +1,42 @@
-export const columns = () => [  
+export const columns = (self) => [
   {
     title: "时间",
     key: "creationTime",
     align: "left",
-    width: 200
+    width: 200,
   },
   {
     title: "IP地址",
     key: "sourceIp",
     align: "center",
-    width: 200
+    width: 200,
   },
   {
     title: "操作",
     key: "content",
-    align: "center"
+    align: "center",
+    render: (h, { row }) => {
+      return h(
+        "div",
+        [row.content, h("Icon", {
+          style: {
+            "vertical-align": " baseline",
+            paddingLeft: "10px"
+          },
+          props: {
+            custom: "icon-info"
+          },
+          on: {
+            click: () => self.handleOpenMessage(row)
+          }
+        })]
+      );
+    },
   },
   {
     title: "结果",
     key: "opperText",
     align: "right",
-    width: 200
-  }
+    width: 200,
+  },
 ];
